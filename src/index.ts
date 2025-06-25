be7289f9b7fc2a9d68bc5f3c5d8a367da393e8bc
rogerio.anselmo <Rogerio.Anselmo-ext@ab-Inbev.com>
2025-06-25 13:19:17 -0300

    git ignore

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('working');
+  const repo = process.env.ENV_REPO
+  res.send(`working from ${repo}`);
 });
 
 app.listen(port, () => {
